b269d5a3ee0bb7df2e494f5cfe3e4836b4f2394a
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-01-24 14:45:47 +0500

    style: admin style improvements

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,31 +1,63 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const AdminDashboard = () => {
   return (
     <div className="flex-1 space-y-8 pt-6">
       {/* stats */}
       <div className="grid gap-4 md:grid-cols-3">
-        <div className="rounded-lg border bg-card p-6">
+        <div className="flex flex-col justify-between rounded-lg border bg-card p-6 h-[130px]">
           <div className="flex items-center gap-2">
-            <h3 className="font-semibold">Borrowed Books</h3>
-            <span className="text-xs">↓ 2</span>
+            <h3 className="text-lg font-semibold text-muted-foreground">
+              Borrowed Books
+            </h3>
+            <span className="text-sm flex items-center justify-center gap-1 text-red-500 font-bold">
+              <Image
+                src="/icons/admin/caret-down.svg"
+                alt="up"
+                width={15}
+                height={15}
+              />
+              2
+            </span>
           </div>
-          <p className="text-2xl font-bold">145</p>
+          <p className="text-3xl font-bold">145</p>
         </div>
-        <div className="rounded-lg border bg-card p-6">
+        <div className="flex flex-col justify-between rounded-lg border bg-card p-6 h-[130px]">
           <div className="flex items-center gap-2">
-            <h3 className="font-semibold">Total Users</h3>
-            <span className="text-xs">↑ 4</span>
+            <h3 className="text-lg font-semibold text-muted-foreground">
+              Total Users
+            </h3>
+            <span className="text-sm flex items-center justify-center gap-1 text-green-500 font-bold">
+              <Image
+                src="/icons/admin/caret-up.svg"
+                alt="up"
+                width={15}
+                height={15}
+              />
+              10
+            </span>
           </div>
-          <p className="text-2xl font-bold">317</p>
+          <p className="text-3xl font-bold">283</p>
         </div>
-        <div className="rounded-lg border bg-card p-6">
+        <div className="flex flex-col justify-between rounded-lg border bg-card p-6 h-[130px]">
           <div className="flex items-center gap-2">
-            <h3 className="font-semibold">Total Books</h3>
-            <span className="text-xs">↑ 2</span>
+            <h3 className="text-lg font-semibold text-muted-foreground">
+              Total Books
+            </h3>
+            <span className="text-sm flex items-center justify-center gap-1 text-red-500 font-bold">
+              <Image
+                src="/icons/admin/caret-down.svg"
+                alt="up"
+                width={15}
+                height={15}
+              />
+              2
+            </span>
           </div>
-          <p className="text-2xl font-bold">163</p>
+          <p className="text-3xl font-bold">120</p>
         </div>
       </div>
 
@@ -35,7 +67,7 @@ const AdminDashboard = () => {
           {/* borrow requests */}
           <div className="rounded-lg border bg-card">
             <div className="flex items-center justify-between p-6">
-              <h3 className="font-semibold">Borrow Requests</h3>
+              <h3 className="font-semibold text-xl">Borrow Requests</h3>
               <a href="#" className="text-sm text-blue-600 hover:underline">
                 View all
               </a>
@@ -57,7 +89,10 @@ const AdminDashboard = () => {
                   user: "Daniel Steward",
                 },
               ].map((book) => (
-                <div key={book.title} className="flex items-start gap-4">
+                <div
+                  key={book.title}
+                  className="flex items-start gap-4 bg-light-300 rounded-lg p-4"
+                >
                   <div className="h-16 w-12 bg-muted rounded">
                     <Image
                       src="https://placehold.co/64x96"
@@ -95,13 +130,13 @@ const AdminDashboard = () => {
           {/* account requests */}
           <div className="rounded-lg border bg-card">
             <div className="flex items-center justify-between p-6">
-              <h3 className="font-semibold">Account Requests</h3>
+              <h3 className="font-semibold text-xl">Account Requests</h3>
               <a href="#" className="text-sm text-blue-600 hover:underline">
                 View all
               </a>
             </div>
             <div className="p-6 pt-0">
-              <div className="grid gap-6">
+              <div className="grid gap-6 grid-cols-3">
                 {[
                   {
                     name: "Marc Atanson",
@@ -119,7 +154,10 @@ const AdminDashboard = () => {
                     initials: "RR",
                   },
                 ].map((user) => (
-                  <div key={user.name} className="flex items-center gap-4">
+                  <div
+                    key={user.name}
+                    className="flex flex-col text-center items-center gap-4 bg-light-300 rounded-lg p-4"
+                  >
                     <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-blue-100 text-sm font-semibold">
                       {user.initials}
                     </span>
@@ -127,7 +165,7 @@ const AdminDashboard = () => {
                       <h4 className="text-sm font-medium leading-none">
                         {user.name}
                       </h4>
-                      <p className="text-sm text-muted-foreground">
+                      <p className="text-sm text-muted-foreground truncate max-w-[130px]">
                         {user.email}
                       </p>
                     </div>
@@ -140,13 +178,28 @@ const AdminDashboard = () => {
 
         {/* Column 2: Recently added books */}
         <div className="rounded-lg border bg-card">
-          <div className="flex items-center justify-between p-6">
-            <h3 className="font-semibold">Recently Added Books</h3>
+          <div className="flex items-center justify-between p-6 pb-0">
+            <h3 className="font-semibold text-xl">Recently Added Books</h3>
             <a href="#" className="text-sm text-blue-600 hover:underline">
               View all
             </a>
           </div>
-          <div className="space-y-4 p-6 pt-0">
+          <div className="space-y-5 p-6 pt-0">
+            <Link href="/admin/books/new">
+              <div className="add-new-book_btn">
+                <div className="">
+                  <Image
+                    src="/icons/admin/plus.svg"
+                    alt="add book"
+                    height={20}
+                    width={20}
+                  />
+                </div>
+                <p className="font-semibold text-lg text-dark-400">
+                  Add New Book
+                </p>
+              </div>
+            </Link>
             {[
               {
                 title: "Inside Evil: Inside Evil Series, Book 1",
