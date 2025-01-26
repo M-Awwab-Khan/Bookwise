4a673dd4aec5b50bcc5ef7740c1bb831603936be
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-01-26 12:34:31 +0500

    feat: admin books page

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Trash2, ExternalLink } from "lucide-react";
+import { format } from "date-fns";
+import { fetchBooks } from "@/lib/actions/admin";
+import BookCover from "@/components/BookCover";
 
-const Page = () => {
+const Page = async () => {
+  const booksData = await fetchBooks();
   return (
     <section className="w-full rounded-2xl bg-white p-7">
       <div className="flex flex-wrap items-center justify-between gap-2">
@@ -15,7 +28,59 @@ const Page = () => {
       </div>
 
       <div className="mt-7 w-full overflow-hidden">
-        <p>Table</p>
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-light-300 h-[50px]">
+              <TableHead>Book Title</TableHead>
+              <TableHead>Author</TableHead>
+              <TableHead>Genre</TableHead>
+              <TableHead>Date Issued</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {booksData.map((book) => (
+              <TableRow key={book.id} className="h-[70px]">
+                <TableCell className="font-medium">
+                  <div className="flex items-center gap-3">
+                    <BookCover
+                      coverColor={book.coverColor}
+                      coverImage={book.coverUrl}
+                      variant="small"
+                    />
+                    <div className="flex flex-col">
+                      <span>{book.title}</span>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>{book.genre}</TableCell>
+                <TableCell>{format(book.createdAt, "yyyy-MM-dd")}</TableCell>
+
+                <TableCell>
+                  <div className="flex gap-2">
+                    <Button variant={"ghost"} asChild>
+                      <Link
+                        href={`/admin/books/${book.id}`}
+                        className="text-primary-admin"
+                      >
+                        <ExternalLink size={16} />
+                      </Link>
+                    </Button>
+                    <Button variant="ghost" asChild>
+                      <Link
+                        href={`/admin/books/${book.id}/delete`}
+                        className="text-red"
+                      >
+                        <Trash2 size={16} />
+                      </Link>
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </section>
   );
--- lib/actions/admin.ts
+++ lib/actions/admin.ts
@@ -1,6 +1,6 @@
 "use server";
 import { db } from "@/database/drizzle";
-import { borrowRecords, users } from "@/database/schema";
+import { books, borrowRecords, users } from "@/database/schema";
 import { eq, sql } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
@@ -26,4 +26,9 @@ type ROLE = "USER" | "ADMIN";
 export const updateUserRole = async (userId: string, role: ROLE) => {
   await db.update(users).set({ role }).where(eq(users.id, userId));
   revalidatePath("/admin/users");
+};
+
+export const fetchBooks = async () => {
+  const booksData = await db.select().from(books);
+  return booksData;
 };
